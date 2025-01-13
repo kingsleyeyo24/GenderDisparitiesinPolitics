e68023f0fbcc22b1077f0e43f94af027d83f13c8
kingsleyeyo24 <113740530+kingsleyeyo24@users.noreply.github.com>
2025-01-13 16:26:06 +0100

    Add files via upload

---
+++ SQLQueryforgender&politics.sql
@@ -0,0 +1,141 @@
+SELECT *
+FROM dbo.GenderandPolitics;
+
+SELECT Position
+FROM dbo.GenderandPolitics
+WHERE Gender = 'Female';
+
+UPDATE dbo.GenderandPolitics
+SET Position = REPLACE(position, 'Commissioner of ', 'Commissioner')
+WHERE Position LIKE 'Commissioner of %';
+
+UPDATE dbo.GenderandPolitics
+SET Position = 'Commissioner'
+WHERE Position LIKE 'Commissioner%';
+
+SELECT *
+FROM dbo.GenderandPolitics;
+
+UPDATE dbo.GenderandPolitics
+SET Position = 'Special Adviser'
+WHERE Position LIKE 'Special Adviser%%';
+
+UPDATE dbo.GenderandPolitics
+SET Position = 'Commissioner'
+WHERE Position LIKE 'Comissioner';
+
+WITH CTE AS (
+    SELECT 
+        ROW_NUMBER() OVER (ORDER BY (SELECT NULL)) AS RowNum,
+        *
+    FROM dbo.GenderandPolitics
+)
+UPDATE CTE
+SET Position = 'Commissioner'
+WHERE RowNum BETWEEN 152 AND 176;
+
+SELECT Gender, State
+FROM dbo.GenderandPolitics
+
+SELECT State,
+COUNT(*) AS female_count
+FROM dbo.GenderandPolitics
+WHERE Gender = 'Female'
+GROUP BY State;
+
+SELECT Position,
+COUNT(*) AS female_count
+FROM dbo.GenderandPolitics
+WHERE Gender = 'Female'
+GROUP BY Position;
+
+SELECT DISTINCT Position
+FROM dbo.GenderandPolitics;
+
+UPDATE dbo.GenderandPolitics
+SET Position = CASE
+    WHEN position NOT IN ('Chairman', 'Commissioner', 'Senator', 'House of Representatives', 'Governor', 'Deputy Governor') 
+	THEN 'Other High Ranking Officials'
+    ELSE Position
+END;
+
+SELECT Position, State, 
+COUNT(*) AS female_count
+FROM dbo.GenderandPolitics
+WHERE Gender = 'Female'
+GROUP BY Position, State;
+
+SELECT Gender, Position, State
+FROM dbo.GenderandPolitics
+Where Gender = 'Female'
+GROUP BY Gender, Position, State;
+
+
+ALTER TABLE dbo.GenderandPolitics
+DROP COLUMN Party;
+
+SELECT *
+FROM dbo.GenderandPolitics
+
+SELECT * 
+FROM dbo.GenderandPolitics
+WHERE [Literacy Rate] IS NOT NULL
+ORDER BY [Literacy Rate] DESC;
+
+SELECT State,
+COUNT(*) AS FemaleCount
+FROM dbo.GenderandPolitics
+WHERE Gender = 'Female'
+GROUP BY State
+ORDER BY FemaleCount DESC;
+
+SELECT Position,
+COUNT (*) FemaleCount
+FROM dbo.GenderandPolitics
+WHERE GENDER = 'Female'
+GROUP BY Position
+ORDER BY FemaleCount DESC;
+
+SELECT 
+    Gender, 
+    COUNT(*) AS count
+FROM 
+    dbo.GenderandPolitics
+GROUP BY 
+    Gender;
+
+WITH GenderCount AS (
+    SELECT 
+        State,
+        Gender,
+        COUNT(*) AS count
+    FROM 
+        dbo.GenderandPolitics
+    GROUP BY 
+        State, Gender
+),
+TotalCount AS (
+    SELECT 
+        State,
+        SUM(count) AS total_positions,
+        SUM(CASE WHEN Gender = 'Female' THEN count ELSE 0 END) AS female_count
+    FROM 
+        GenderCount
+    GROUP BY 
+        State
+)
+SELECT 
+    State,
+    female_count,
+    total_positions,
+    ROUND((CAST(female_count AS FLOAT) / total_positions) * 100, 2) AS female_percentage
+FROM 
+    TotalCount
+ORDER BY 
+    female_percentage DESC;
+
+SELECT *
+FROM [PortfolioProject].[dbo].[GenderandPolitics]
+WHERE [Education Budget] IS NOT NULL
+
+
